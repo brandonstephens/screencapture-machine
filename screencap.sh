2a970f8f10c025ae0f5885030e1aaacfe185328a
Brandon Stephens <brandon.stephens@generationtux.com>
2016-07-22 12:46:34 -0400

    refactor

--- screencap.sh
+++ screencap.sh
@@ -2,28 +2,30 @@
 #set -o nounset
 #set -o errexit
 
-LOCAL_ROOT=`pwd`
-CAPTURE_DIR=captures
+# LOCAL_ROOT=`.`
+CAPTURE_DIR='./captures'
 
 # make dir if it doesn't exist
-if [ ! -d "$LOCAL_ROOT/$CAPTURE_DIR" ]; then
-  mkdir "$LOCAL_ROOT/$CAPTURE_DIR"
+if [ ! -d "$CAPTURE_DIR" ]; then
+  mkdir "$CAPTURE_DIR"
 fi
 
 START=$(date +"%s")
 START_DIR=$CAPTURE_DIR/`date '+%Y-%m-%d_%H.%M.%S'`
 
 # append a line return to make sure last line is processed
-sed -i '' -e '$a\' $LOCAL_ROOT/urls.txt
+sed -i '' -e '$a\' urls.txt
 
 mkdir $START_DIR
-cd $START_DIR
+# cd $START_DIR
 
-while read in; do phantomjs $LOCAL_ROOT/lib/responsive-screenshot.js "$in" png false; done < $LOCAL_ROOT/urls.txt
+while read in; do 
+    phantomjs ./lib/responsive-screenshot.js "$in" png false "$START_DIR/"; 
+done < ./urls.txt
 
-cd $LOCAL_ROOT
+# cd $LOCAL_ROOT
 
-mv $LOCAL_ROOT/urls.txt $START_DIR
+mv ./urls.txt $START_DIR
 
 END=$(date +"%s")
 DIFF=$(($END-$START))
